3ffe256756f8b451144a05da48d7fcf5a1c152bf
muku <mukulbadhan@gmail.com>
2021-10-10 17:30:36 -0700

    complete

--- gradient_descent.py
+++ gradient_descent.py
@@ -1,10 +1,12 @@
 import numpy as np
+
+
 def gradient_descent(gradiant, init_x, n):
-    for i in range(52):
+    for i in range(50000):
         grad = gradiant(init_x)
+
         new_x = np.subtract(init_x, n * grad)
-        #print(i, new_x, init_x, grad)
-        if grad.all() == 0:
+        if np.all(grad < 0.0001):
             break
         init_x = new_x
 
--- perceptron.py
+++ perceptron.py
@@ -4,6 +4,7 @@ import numpy as np
 def perceptron_train(X, Y):
     w = np.array([0 for i in X[0]])
     b = 0
+    max_epock=10000
     while True:
         epic_without_update = 0
         for i in range(len(Y)):
@@ -13,9 +14,12 @@ def perceptron_train(X, Y):
                 w = np.add(w, np.multiply(X[i], Y[i]))
                 b = b + Y[i]
                 epic_without_update = epic_without_update + 1
-
+        max_epock=max_epock-1
         if epic_without_update == 0:
             break
+        if max_epock==0:
+            print("data not linearly convergerable. max epoch limit has reached")
+            return np.array([0 for i in X[0]]),0
 
     return w, b
 
@@ -27,6 +31,7 @@ def perceptron_test(X, Y, w, b):
     for i in range(sample_size):
         activation = np.multiply(X[i], w)
         activation = np.sum(activation) + b
+        #print(activation)
         y_pred = -1 if activation <= 0 else 1
         if y_pred == Y[i]:
             accuracy = accuracy + 1
